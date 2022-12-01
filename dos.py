082c2a80345346dcbad4130e31826929f8bfa270
John <mwxfranco@gmail.com>
2022-12-01 16:56:47 +0000

    moved the argument parser to dos.py to make the dos.py file easier to use in other projects

--- dos.py
+++ dos.py
@@ -1,4 +1,27 @@
-from dos_argumentparser import DOS_ArgumentParser
+class DOS_ArgumentParser:
+    def __init__(self):
+        self.args = []
+
+    def parse(self, string_args: str):
+        self.args = []
+        args = string_args.split(" ")
+        for arg in args:
+            parsed_argument = self._parse_single_argument(arg)
+            self.args.append(parsed_argument)
+        return self.args
+
+    def _parse_single_argument(self, arg):
+        parsed_arg = {
+            'name': None,
+            'value': None
+        }
+        if '=' in arg:
+            pair = arg.split('=', 1)
+            parsed_arg['name'] = pair[0]
+            parsed_arg['value'] = pair[1]
+        else:
+            parsed_arg['value'] = arg
+        return parsed_arg
 
 
 class DOS_Command:
@@ -34,7 +57,7 @@ class DOS_Command:
         else:
             self.optional_args.append(arg)
 
-        #self.arguments.append(arg)
+        # self.arguments.append(arg)
 
     def generate_syntax(self):
         '''
